9dbd4e8b30e850deaf308daae87498a3d53bf762
Ahmad Obiedat <obiedatdeveloper@gmail.com>
2018-10-10 21:11:45 -0400

    day 3 practice

--- first-react-app/src/index.js
+++ first-react-app/src/index.js
@@ -19,3 +19,16 @@ var bill=10.25 + 3.99 + 7.15;
 var tip=bill*0.15;
 var total = bill +tip;
 console.log(total);
+var celisus=12;
+var fahrenheit= celisus*1.8 +32;
+
+console.log(fahrenheit);
+var thingOne="red"; var thingTwo="blue";
+console.log(thingOne,thingTwo);
+var fullName="Ahmad Obiedat";
+console.log(fullName);
+
+var bill=10.25 + 3.99 + 7.15;
+var tip=bill*0.15;
+var total = bill +tip;
+console.log(total);
