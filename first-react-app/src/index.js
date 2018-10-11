8a0f66c2ee20809219f68e4fc36b9d397f2ca9ff
Ahmad Obiedat <obiedatdeveloper@gmail.com>
2018-10-11 17:09:38 -0400

    day 4 practice

--- first-react-app/src/index.js
+++ first-react-app/src/index.js
@@ -32,3 +32,32 @@ var bill=10.25 + 3.99 + 7.15;
 var tip=bill*0.15;
 var total = bill +tip;
 console.log(total);
+// Setup
+var a;
+var b = 2;
+
+// Only change code below this line
+// Example
+var ourVar = 19;
+
+// Only change code below this line
+var a = 9;
+// Initialize these three variables
+var a =5;
+var b = 10;
+var c = "I am a";
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+// Declarations
+var StUdLyCapVaR;
+var properCamelCase;
+var TitleCaseOver;
+
+// Assignments
+STUDLYCAPVAR = 10;
+PRoperCAmelCAse = "A String";
+tITLEcASEoVER = 9000;
