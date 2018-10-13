eb369b2803721de4b5dc192ac779355ebad68d20
Ahmad Obiedat <obiedatdeveloper@gmail.com>
2018-10-13 12:11:45 -0400

    day6  practice

--- first-react-app/src/index.js
+++ first-react-app/src/index.js
@@ -565,3 +565,46 @@ function testStrictNotEqual(val) {
   
   // Change this value to test
   orderMyLogic(7);  
+  function testSize(num) {
+    // Only change code below this line
+    if(num <5)
+    {return "Tiny";
+    }else if(num <10){
+      return "Small";
+    }else if(num <15)
+    {
+      return "Medium";
+    }else if(num <20){
+      return "Large";
+    }else if(num>=20){
+      return "Huge";
+    }
+    
+  
+  }
+  
+  // Change this value to test
+  testSize(7);
+  var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+function golfScore(par, strokes) {
+  // Only change code below this line
+  if(strokes===1){
+    return names[0];
+  }else if(strokes <=par-2){
+    return names[1];
+  }else if(strokes ===par-1){
+    return names[2];
+  }else if (strokes ===par){
+    return names[3];
+  }else if (strokes ===par+1){
+    return names[4];
+  }else if (strokes ===par+2){
+    return names[5];
+  }else if (strokes >=par+3){
+    return names[6];
+  }
+ 
+}
+
+// Change these values to test
+golfScore(5, 4);
