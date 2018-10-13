cdb22606c4efe1c7d39d4fffc50ad9e1949f15bf
Ahmad Obiedat <obiedatdeveloper@gmail.com>
2018-10-13 03:33:22 -0400

    day6  practice

--- first-react-app/src/index.js
+++ first-react-app/src/index.js
@@ -404,3 +404,164 @@ function compareEquality(a, b) {
   
   // Change this value to test
   compareEquality(10, "10");
+
+  // Setup
+function testNotEqual(val) {
+    if (val !=99) { // Change this line
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+  
+  // Change this value to test
+  testNotEqual(10);
+
+  // Setup
+function testStrictNotEqual(val) {
+    // Only Change Code Below this Line
+    
+    if (val!==17) {
+  
+    // Only Change Code Above this Line
+  
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+  
+  // Change this value to test
+  testStrictNotEqual(10);
+
+  function testGreaterThan(val) {
+    if (val>100) {  // Change this line
+      return "Over 100";
+    }
+    
+    if (val > 10) {  // Change this line
+      return "Over 10";
+    }
+  
+    return "10 or Under";
+  }
+  
+  // Change this value to test
+  testGreaterThan(10);
+  function testGreaterOrEqual(val) {
+    if (val >=20) {  // Change this line
+      return "20 or Over";
+    }
+    
+    if (val>=10) {  // Change this line
+      return "10 or Over";
+    }
+  
+    return "Less than 10";
+  }
+  
+  // Change this value to test
+  testGreaterOrEqual(10);
+
+  function testLessThan(val) {
+    if (val<25) {  // Change this line
+      return "Under 25";
+    }
+    
+    if (val<55) {  // Change this line
+      return "Under 55";
+    }
+  
+    return "55 or Over";
+  }
+  
+  // Change this value to test
+  testLessThan(10);
+  function testLessOrEqual(val) {
+    if (val <=12) {  // Change this line
+      return "Smaller Than or Equal to 12";
+    }
+    
+    if (val<=24) {  // Change this line
+      return "Smaller Than or Equal to 24";
+    }
+  
+    return "More Than 24";
+  }
+  
+  // Change this value to test
+  testLessOrEqual(10);
+  function testLogicalAnd(val) {
+    // Only change code below this line
+  
+    if (val<=50 && val >=25) {
+  
+        return "Yes";
+      
+    }
+  
+    // Only change code above this line
+    return "No";
+  }
+  
+  // Change this value to test
+  testLogicalAnd(10);
+  function testLogicalOr(val) {
+    // Only change code below this line
+  
+    if (val >20|| val <10) {
+      return "Outside";
+    }
+  
+   
+  
+    // Only change code above this line
+    return "Inside";
+  }
+  
+  // Change this value to test
+  testLogicalOr(15);
+  function testElse(val) {
+    var result = "";
+    // Only change code below this line
+    
+    if (val > 5) {
+      result = "Bigger than 5";
+    }
+    
+    else{
+      result = "5 or Smaller";
+    }
+    
+    // Only change code above this line
+    return result;
+  }
+  
+  // Change this value to test
+  testElse(4);
+  function testElseIf(val) {
+    if (val > 10) {
+      return "Greater than 10";
+    }
+    
+   else if (val < 5) {
+      return "Smaller than 5";
+    }
+    else{
+    return "Between 5 and 10";
+  
+    }
+  }
+  
+  // Change this value to test
+  testElseIf(7);
+  function orderMyLogic(val) {
+    if (val < 5) {
+      return "Less than 5";
+    } else if (val < 10) {
+      return "Less than 10";
+    } else {
+      return "Greater than or equal to 10";
+    }
+  }
+  
+  // Change this value to test
+  orderMyLogic(7);  
