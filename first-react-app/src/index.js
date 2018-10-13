98fce041b9ba1f61abe2990f4b07e1e7741fb603
Ahmad Obiedat <obiedatdeveloper@gmail.com>
2018-10-13 01:25:27 -0400

    day6  practice

--- first-react-app/src/index.js
+++ first-react-app/src/index.js
@@ -249,3 +249,158 @@ function fun2() {
   }
   console.log(output);
 }
+  function myLocalScope() {
+    'use strict'; // you shouldn't need to edit this line
+    var myVar;  
+    console.log(myVar);
+  }
+  myLocalScope();
+    var myVar;  
+  
+  // Run and check the console
+  // myVar is not defined outside of myLocalScope
+  console.log(myVar);
+  
+  // Now remove the console log line to pass the test
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  
+  var outerWear = "sweater";
+  
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
+
+// Example
+function minusSeven(num) {
+    return num - 7;
+  }
+  
+  // Only change code below this line
+  function timesFive(args){
+    return args*5;
+  }
+  
+  
+  console.log(minusSeven(10));
+  console.log(timesFive(5));
+
+  // Example
+var sum = 0;
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive (){
+  sum+=5;
+}
+
+
+// Only change code above this line
+var returnedValue = addFive();
+// Example
+var changed = 0;
+
+function change(num) {
+  return (num + 5) / 3;
+}
+
+changed = change(10);
+
+// Setup
+var processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+
+var processed = processArg(7);
+function nextInLine(arr, item) {
+    // Your code here
+    arr.push(item);
+    var removedItem = arr.shift();
+    return removedItem;  // Change this line
+  }
+  
+  // Test Setup
+  var testArr = [1,2,3,4,5];
+  
+  // Display Code
+  console.log("Before: " + JSON.stringify(testArr));
+  console.log(nextInLine(testArr, 6)); // Modify this line to test
+  console.log("After: " + JSON.stringify(testArr));
+
+
+  // Example
+function ourTrueOrFalse(isItTrue) {
+    if (isItTrue) { 
+      return "Yes, it's true";
+    }
+    return "No, it's false";
+  }
+  
+  // Setup
+  function trueOrFalse(wasThatTrue) {
+  
+    // Only change code below this line.
+    if(wasThatTrue)
+     {
+         return "Yes, that was true";
+     }
+    return "No, that was false";
+    
+    // Only change code above this line.
+  
+  }
+  
+  // Change this value to test
+  trueOrFalse(true);
+  // Setup
+function testEqual(val) {
+    if (val==12) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  // Change this value to test
+  testEqual(10);
+  // Setup
+function testStrict(val) {
+    if (val===7) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  // Change this value to test
+  testStrict(10);
+
+  // Setup
+function compareEquality(a, b) {
+    if (a === b) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  // Change this value to test
+  compareEquality(10, "10");
+  // Setup
+function compareEquality(a, b) {
+    if (a === b) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  // Change this value to test
+  compareEquality(10, "10");
